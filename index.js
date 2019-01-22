8702d12e0a80484375c1fc712aa19a55034aeaf9
FesenkoE <fesenkoe@ukr.net>
2019-01-23 00:43:41 +0200

    send mail

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('Hello, world');
+// console.log('Hello, world');
+
+// const fs = require('fs');
+//
+// fs.readFile('text.txt', 'UTF-8', function (err, data) {
+//     console.log(data);
+// });
+//
+// fs.writeFile('message.txt', 'Hello Nod', 'utf8', function (err, data) {
+//     console.log('The file has been saved');
+// });
+//
+// fs.readFile('message.txt', 'UTF-8', function (err, data) {
+//     console.log(data);
+// });
+//
+// fs.appendFile('message.txt', '\nHello Node.js', 'utf8', function (error) {
+//     console.log('The string has been append');
+// });
+
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'fesenkoe2018@gmail.com',
+        pass: 'mypassword'
+    }
+});
+
+var mailOptions = {
+    from: 'fesenkoe2018@gmail.com',
+    to: 'fesenkoe2018@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
